3fbba9cfe5904f0ea0daccb050d19b2368dc3cf7
cgalatro2 <chasegalatro@gmail.com>
2023-08-28 22:20:07 -0400

    add and config react query and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,60 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { registerRootComponent } from "expo";
-import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home, User } from "react-native-feather";
+import axios from "axios";
 
 import HomeScreen from "@screens/HomeScreen";
 import ProfileScreen from "@screens/ProfileScreen";
 
 const Tab = createBottomTabNavigator();
 
+const defaultQueryFn = async ({ queryKey }) => {
+  const { data } = await axios.get(
+    `https://random-data-api.com/api/v2/${queryKey[0]}?size=5`
+  );
+  return data;
+};
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: defaultQueryFn,
+    },
+  },
+});
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused }) => {
-            const strokeWidth = focused ? 3 : 1;
-            return route.name === "Profile" ? (
-              <User strokeWidth={strokeWidth} />
-            ) : (
-              <Home strokeWidth={strokeWidth} />
-            );
-          },
-          tabBarShowLabel: false,
-        })}
-      >
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Feed" }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{ title: "Profile" }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused }) => {
+              const strokeWidth = focused ? 3 : 1;
+              return route.name === "Profile" ? (
+                <User strokeWidth={strokeWidth} />
+              ) : (
+                <Home strokeWidth={strokeWidth} />
+              );
+            },
+            tabBarShowLabel: false,
+          })}
+        >
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: "Feed" }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{ title: "Profile" }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,36 @@
 import { Text } from "react-native";
+import { useQuery } from "react-query";
+
+type Beer = {
+  id: number;
+  uid: string;
+  brand: string;
+  name: string;
+  style: string;
+  hop: string;
+  yeast: string;
+  malts: string;
+  ibu: string;
+  alcohol: string;
+  blg: string;
+};
 
 export default function HomeScreen() {
-  return <Text>Heyo!</Text>;
+  const { isLoading, error, data } = useQuery<Beer[]>("beers");
+
+  if (isLoading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (error) {
+    return <Text>{`${error}`}</Text>;
+  }
+
+  return (
+    <>
+      {data?.map((beer) => {
+        return <Text key={beer.id}>{beer.name}</Text>;
+      })}
+    </>
+  );
 }
