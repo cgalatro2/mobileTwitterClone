f3b2c6d3e1bf74b5610663fc8e8e4b3b4e0246a5
cgalatro2 <chasegalatro@gmail.com>
2023-09-20 00:11:50 -0400

    added a loading screen for local login

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -3,40 +3,54 @@ import { View, Button } from "react-native";
 import { Input } from "@rneui/themed";
 
 import { useAuthDispatch, login, tryLocalLogin } from "context/AuthContext";
+import LoadingScreen from "./LoadingScreen";
 
 export default function LoginScreen({ navigation }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isTryingLogin, setIsTryingLogin] = useState(false);
 
   const dispatch = useAuthDispatch();
 
+  const tryLogin = async () => {
+    setIsTryingLogin(true);
+    await tryLocalLogin(dispatch);
+    setIsTryingLogin(false);
+  };
+
   useEffect(() => {
-    tryLocalLogin(dispatch);
+    tryLogin();
   }, []);
 
   return (
-    <View>
-      <Input
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-      />
-      <Input
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        autoCapitalize="none"
-        secureTextEntry
-      />
-      <Button
-        title="Sign in"
-        onPress={() => login(dispatch, { email, password })}
-      />
-      <Button
-        title="Don't have an account? Sign up"
-        onPress={() => navigation.navigate("Signup")}
-      />
-    </View>
+    <>
+      {isTryingLogin ? (
+        <LoadingScreen />
+      ) : (
+        <View>
+          <Input
+            placeholder="Email"
+            value={email}
+            onChangeText={setEmail}
+            autoCapitalize="none"
+          />
+          <Input
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            autoCapitalize="none"
+            secureTextEntry
+          />
+          <Button
+            title="Sign in"
+            onPress={() => login(dispatch, { email, password })}
+          />
+          <Button
+            title="Don't have an account? Sign up"
+            onPress={() => navigation.navigate("Signup")}
+          />
+        </View>
+      )}
+    </>
   );
 }
